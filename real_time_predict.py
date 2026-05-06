49ffe76873c2df3af128968e733b247bdf1b61a1
damilola ayodeji <damilolayodeji@gmail.com>
2026-05-06 08:48:14 +0000

    Add sentence builder and confidence filtering

--- real_time_predict.py
+++ real_time_predict.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import joblib
 import numpy as np
+import time
 
 model = joblib.load("gsl_model.pkl")
 label_encoder = joblib.load("label_encoder.pkl")
@@ -20,6 +21,10 @@ cap = cv2.VideoCapture(0)
 
 sentence = ""
 last_prediction = ""
+last_added_time = 0
+
+confidence_threshold = 80
+delay_seconds = 1.2
 
 while True:
     success, frame = cap.read()
@@ -48,6 +53,17 @@ while True:
             else:
                 confidence = 0
 
+            current_time = time.time()
+
+            if (
+                confidence >= confidence_threshold
+                and gesture != last_prediction
+                and current_time - last_added_time >= delay_seconds
+            ):
+                sentence += gesture
+                last_prediction = gesture
+                last_added_time = current_time
+
             mp_draw.draw_landmarks(
                 frame,
                 hand_landmarks,
@@ -64,10 +80,36 @@ while True:
                 2
             )
 
+    cv2.putText(
+        frame,
+        f"Text: {sentence}",
+        (10, 100),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 255, 255),
+        2
+    )
+
+    cv2.putText(
+        frame,
+        "Press C to clear | Press Q to quit",
+        (10, 450),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.7,
+        (255, 255, 255),
+        2
+    )
+
     cv2.imshow("GSL Real-Time Prediction", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
         break
+
+    if key == ord("c"):
+        sentence = ""
+        last_prediction = ""
 
 cap.release()
 cv2.destroyAllWindows()
